c097ebedaf12142e825e5034c2cc487656d06d0a
thomas <thomas.formiga2@gmail.com>
2022-01-16 16:03:24 +0100

    too many commits

---
+++ bot.py
@@ -0,0 +1,32 @@
+import os
+import discord
+from discord.ext import commands
+
+#Bot parameters
+bot = commands.Bot(
+    activity=discord.Game(name='/jester, /help'),
+    command_prefix=commands.when_mentioned_or("j! "),
+)
+#guild list (not working yet)
+guild_ids = []
+for guild in bot.guilds:
+    guild_ids.append(guild.id)
+
+#Connection confirmation
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+
+
+
+#Commands
+@bot.slash_command(guild_ids=[928401895595327548], name="jester")
+async def jester(ctx):
+    """Say hi to Jester!""" 
+    await ctx.respond(f"Hello {ctx.author.mention}, I am ***jester.***\nI have endless games, charades, prizes and rewards...\n\nJust type `/game` and we can begin...")
+
+
+
+
+#All done, run token
+bot.run(os.environ['TOKEN'])
