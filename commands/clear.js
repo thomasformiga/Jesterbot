4e4efd5b882f5bd3311080ed3dda67e7157efa0a
thomas <thomas.formiga2@gmail.com>
2022-01-18 15:56:35 +0100

    now in JS

---
+++ commands/clear.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('clear')
+		.setDescription('delete up to 99 messages')
+		.addIntegerOption(option => option.setName('amount').setDescription('number of messages to delete')),
+	async execute(interaction) {
+		const amount = interaction.options.getInteger('amount');
+
+		if (amount <= 1 || amount > 100) {
+			return interaction.reply({ content: 'i need a number between 1 and 99 idiot', ephemeral: true });
+		}
+		await interaction.channel.bulkDelete(amount, true).catch(error => {
+			console.error(error);
+			interaction.reply({ content: 'something went wrong lmao', ephemeral: true });
+		});
+
+		return interaction.reply({ content: `cleared \`${amount}\` messages, now lost forever in the endless sands of time`, ephemeral: true });
+	},
+};
--- index.js
+++ index.js
@@ -1,22 +1,44 @@
-const { Client, Intents } = require('discord.js');
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-client.once('ready', () => {
-	console.log('Ready!');
-});
+
+//Read command files
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);
+}
 
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
 
-	const { commandName } = interaction;
+	const command = client.commands.get(interaction.commandName);
 
-	if (commandName === 'jester') {
-		await interaction.reply('Hello @${interaction.author.id}, I am ***jester.***\nI have endless games, charades, prizes and rewards...\n\nType `/help` so we can begin..."');
-	} else if (commandName === 'help') {
-		await interaction.reply('what');
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
 });
+
+//Read event files
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
 
 client.login(token);
