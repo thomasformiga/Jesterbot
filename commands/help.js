f38eafdde2681990e189e2cc40140873036acbb9
thomas <thomas.formiga2@gmail.com>
2022-01-19 15:47:18 +0100

    too many commits

--- commands/help.js
+++ commands/help.js
@@ -1,34 +1,33 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageActionRow, MessageButton } = require('discord.js');
+const { MessageActionRow, MessageButton, MessageSelectMenu} = require('discord.js');
 const { MessageEmbed } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('help')
 		.setDescription('Commands list, Patreon')
-		.addSubcommand(subcommand =>
-            subcommand
-                .setName('commands')
-                .setDescription('Commands list'))
-        .addSubcommand(subcommand =>
-            subcommand
-                .setName('patreon')
-                .setDescription('Sponsor info + Link')),
+        .addStringOption(option =>
+            option.setName('category')
+            .setDescription('options')
+			.setRequired(true)
+			.addChoice('commands', 'Command list')
+			.addChoice('patreon', 'Sponsor me!')),
     async execute(interaction) {
 
-        if (interaction.options.getSubcommand() === 'commands') {
+        
+        if (interaction.options.getBoolean() === 'commands') {
 			await interaction.reply('currently working: /clear');
         };
 
-        if (interaction.options.getSubcommand() === 'patreon') {
+        if (interaction.options.getBoolean() === 'patreon') {
             const PatreonEmbed = new MessageEmbed()
                 .setColor('#e6094f')
                 .setTitle('Become a sponsor!')
                 .setURL('https://patreon.com')
-                .setDescription('Get exclusive benefits, bonuses and goodies!\n||(blood pact no longer required)||')
+                .setDescription('Get exclusive benefits, boosts and goodies!')
                 .setThumbnail('https://i.imgur.com/z7FyPcQ.jpg')
                 .addFields(
-                    { name: 'Benefits:', value: 'lorem ipsum\nsit dolor amet'},
+                    { name: 'Benefits include:', value: 'lorem ipsum\nsit dolor amet'},
                 )
                 .setFooter({ text: '"turns out running an interdimensional circus is ridiculously expensive lmao"', iconURL: 'https://i.imgur.com/TCNO7r1.jpeg' });
 			await interaction.reply({ embeds: [PatreonEmbed] });
